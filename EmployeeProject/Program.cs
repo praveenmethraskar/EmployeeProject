9f4ccf5d46c8f2c04431e5cce299e7554dfc61da
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-03 11:42:41 +0530

    employee present or absent

---
+++ EmployeeProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeProject
+{
+    public class Employee
+    {
+       public static void Main(String[] args)
+        {
+            int Is_Full_Time = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if(empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
