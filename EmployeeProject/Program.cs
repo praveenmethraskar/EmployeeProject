e91bc5a04e848272b987c26056191ff1ac11ced9
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-03 12:17:15 +0530

    UC4_Wageformonth

--- EmployeeProject/Program.cs
+++ EmployeeProject/Program.cs
@@ -4,20 +4,49 @@ namespace EmployeeProject
 {
     public class Employee
     {
-       public static void Main(String[] args)
+        public const int Is_Part_Time = 1;
+        public const int Is_Full_Time = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
+
+        public static void Main(String[] args)
         {
-            int Is_Part_Time = 1;
-            int Is_Full_Time = 2;
-            int EMP_RATE_PER_HOUR = 20;
+            //int Is_Part_Time = 1;
+            //int Is_Full_Time = 2;
+            //int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
+            int totalEmpWage = 0;
 
-            Random random = new Random();
+            for(int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            {
+                Random random = new Random();
 
-            int empCheck = random.Next(0, 3);
+                int empCheck = random.Next(0, 3);
 
-            if(empCheck == Is_Part_Time)
+                switch (empCheck)
+                {
+                    case Is_Part_Time:
+                        empHrs = 4;
+                        break;
+                    case Is_Full_Time:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage " + empWage);
+            }
+            Console.WriteLine("Total Emp Wage" + totalEmpWage);
+            //Random random = new Random();
+
+            //int empCheck = random.Next(0, 3);
+
+           /* if(empCheck == Is_Part_Time)
             {
                 empHrs = 4;
             }
@@ -28,9 +57,9 @@ namespace EmployeeProject
             else
             {
                 empHrs = 0;
-            }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage " + empWage);
+            }*/
+
+
         }
     }
 }
