8c9d30ae76529c0231115f607fb1ba326862f2e9
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-03 12:01:41 +0530

    UC2_EmpWage2

--- EmployeeProject/Program.cs
+++ EmployeeProject/Program.cs
@@ -7,18 +7,25 @@ namespace EmployeeProject
        public static void Main(String[] args)
         {
             int Is_Full_Time = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random();
 
             int empCheck = random.Next(0, 2);
 
             if(empCheck == Is_Full_Time)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage " + empWage);
         }
     }
 }
